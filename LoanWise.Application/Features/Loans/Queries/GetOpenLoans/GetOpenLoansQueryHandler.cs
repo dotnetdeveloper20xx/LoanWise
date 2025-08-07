d7249b8a4e8d44872acd822585c14b2a371f8845
Dotnetdeveloper <dotnetdeveloper20xx@hotmail.com>
2025-08-07 11:33:28 +0100

    refactor: use AutoMapper for Loan → LoanSummaryDto in GetOpenLoansQueryHandler

--- LoanWise.Application/Features/Loans/Queries/GetOpenLoans/GetOpenLoansQueryHandler.cs
+++ LoanWise.Application/Features/Loans/Queries/GetOpenLoans/GetOpenLoansQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using LoanWise.Application.Common.Interfaces;
+﻿using AutoMapper;
+using LoanWise.Application.Common.Interfaces;
 using LoanWise.Application.Features.Loans.DTOs;
 using MediatR;
 using StoreBoost.Application.Common.Models;
@@ -8,26 +9,19 @@ namespace LoanWise.Application.Features.Loans.Queries.GetOpenLoans
     public class GetOpenLoansQueryHandler : IRequestHandler<GetOpenLoansQuery, ApiResponse<List<LoanSummaryDto>>>
     {
         private readonly ILoanRepository _loanRepository;
+        private readonly IMapper _mapper;
 
-        public GetOpenLoansQueryHandler(ILoanRepository loanRepository)
+        public GetOpenLoansQueryHandler(ILoanRepository loanRepository, IMapper mapper)
         {
             _loanRepository = loanRepository;
+            _mapper = mapper;
         }
 
         public async Task<ApiResponse<List<LoanSummaryDto>>> Handle(GetOpenLoansQuery request, CancellationToken cancellationToken)
         {
             var openLoans = await _loanRepository.GetOpenLoansAsync(cancellationToken);
 
-            var result = openLoans.Select(loan => new LoanSummaryDto
-            {
-                LoanId = loan.Id,
-                BorrowerId = loan.BorrowerId,
-                Amount = loan.Amount.Value,
-                FundedAmount = loan.Fundings.Sum(f => f.Amount.Value),
-                DurationInMonths = loan.DurationInMonths,
-                Purpose = loan.Purpose,
-                Status = loan.Status
-            }).ToList();
+            var result = _mapper.Map<List<LoanSummaryDto>>(openLoans);
 
             return ApiResponse<List<LoanSummaryDto>>.SuccessResult(result);
         }
--- LoanWise.Application/Mapping/LoanMappingProfile.cs
+++ LoanWise.Application/Mapping/LoanMappingProfile.cs
@@ -16,6 +16,18 @@ namespace LoanWise.Application.Mapping
                 .ForMember(dest => dest.Purpose, opt => opt.MapFrom(src => src.Purpose))
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
                 .ForMember(dest => dest.RiskLevel, opt => opt.MapFrom(src => src.RiskLevel));
+
+            CreateMap<Loan, LoanSummaryDto>()
+                .ForMember(dest => dest.LoanId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount.Value))
+                .ForMember(dest => dest.FundedAmount, opt => opt.MapFrom(src => src.Fundings.Sum(f => f.Amount.Value)))
+                .ForMember(dest => dest.BorrowerId, opt => opt.MapFrom(src => src.BorrowerId))
+                .ForMember(dest => dest.DurationInMonths, opt => opt.MapFrom(src => src.DurationInMonths))
+                .ForMember(dest => dest.Purpose, opt => opt.MapFrom(src => src.Purpose))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+
+
+
         }
     }
 }
