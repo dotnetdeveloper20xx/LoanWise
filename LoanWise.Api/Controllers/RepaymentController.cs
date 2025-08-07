e874344938d578d3b02e8010b2e1ce1e50750e3b
Dotnetdeveloper <dotnetdeveloper20xx@hotmail.com>
2025-08-07 16:38:43 +0100

    feat: add POST /api/repayments/{repaymentId}/pay endpoint to mark repayment as paid

---
+++ LoanWise.Api/Controllers/RepaymentController.cs
@@ -0,0 +1,33 @@
+﻿
+using LoanWise.Application.Features.Repayments.Commands.MakeRepayment;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanWise.Api.Controllers
+{
+    [ApiController]
+    [Route("api/repayments")]
+    public class RepaymentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RepaymentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Marks a repayment as paid by repayment ID.
+        /// </summary>
+        /// <param name="repaymentId">The ID of the repayment to mark as paid.</param>
+        /// <returns>The repayment ID if successful.</returns>
+        /// <response code="200">Repayment marked as paid.</response>
+        /// <response code="400">Repayment not found or already paid.</response>
+        [HttpPost("{repaymentId}/pay")]
+        public async Task<IActionResult> MakeRepayment(Guid repaymentId)
+        {
+            var result = await _mediator.Send(new MakeRepaymentCommand(repaymentId));
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+    }
+}
