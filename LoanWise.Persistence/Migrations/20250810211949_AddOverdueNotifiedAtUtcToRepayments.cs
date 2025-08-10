6c56517a17f5c42af37aa4c0924c7676d8514e10
Dotnetdeveloper <dotnetdeveloper20xx@hotmail.com>
2025-08-10 22:20:33 +0100

    migrations for AddOverdueNotifiedAtUtcToRepayments

---
+++ LoanWise.Persistence/Migrations/20250810211949_AddOverdueNotifiedAtUtcToRepayments.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanWise.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOverdueNotifiedAtUtcToRepayments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Repayments");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAtUtc",
+                table: "Repayments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OverdueNotifiedAtUtc",
+                table: "Repayments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RejectedAtUtc",
+                table: "Loans",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ApprovedAtUtc",
+                table: "Loans",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAtUtc",
+                table: "Repayments");
+
+            migrationBuilder.DropColumn(
+                name: "OverdueNotifiedAtUtc",
+                table: "Repayments");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Repayments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RejectedAtUtc",
+                table: "Loans",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ApprovedAtUtc",
+                table: "Loans",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
